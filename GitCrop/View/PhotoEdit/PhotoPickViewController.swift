1aba34ea76ea103977f41eb018d6cc9a2fc7aa3a
Geon Kang <kgun90@gmail.com>
2024-09-02 16:29:49 +0900

    refactor: 파일명 변경, 미사용 코드 제거, UI 수정

--- GitCrop/View/PhotoEdit/PhotoPickViewController.swift
+++ GitCrop/View/PhotoEdit/PhotoPickViewController.swift
@@ -14,13 +14,8 @@ protocol PhotoPickDelegate {
 
 
 class PhotoPickViewController: UIViewController {
-    let saveButton: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("완료", for: .normal)
-        btn.setTitleColor(.gray, for: .normal)
-        btn.setTitleColor(.blue, for: .highlighted)
-        return btn
-    }()
+    var saveButton = UIButton()
+    var selectedImageView = EditableImageView()
     
     var imageListView = ImageViewListView()
     private let imageProcessingQueue = DispatchQueue(label: "com.Git.GitCrop.imageProcessingQueue")
@@ -35,24 +30,71 @@ class PhotoPickViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .white
         imageListView.delegate = self
-        saveButton.isEnabled = true
+        buttonSetup()
+        selectedImageViewSetup()
+    }
+    
+    private func buttonSetup() {
+        saveButton.setTitle("완료", for: .normal)
+        saveButton.setTitleColor(.gray, for: .disabled)
+        saveButton.setTitleColor(.blue, for: .normal)
+        saveButton.isEnabled = false
+    }
+    
+    private func selectedImageViewSetup() {
+//        selectedImageView.backgroundColor = .green
     }
     
     private func interface() {
+        addInterface()
+        anchorInterface()
+    }
+    
+    private func addInterface() {
         view.addSubview(saveButton)
+        view.addSubview(selectedImageView)
+        view.addSubview(imageListView)
+    }
+    
+    private func anchorInterface() {
+        buttonAnchor()
+        selectedImageViewAnchor()
+        imageListViewAnchor()
+    }
+    
+    private func buttonAnchor() {
         saveButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(10)
-            $0.trailing.equalToSuperview().inset(10)
+            $0.trailing.equalToSuperview().inset(20)
         }
     }
+    
+    private func selectedImageViewAnchor() {
+        selectedImageView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(50)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(100)
+            $0.height.equalTo(selectedImageView.snp.width)
+            
+        }
+    }
+    
+    private func imageListViewAnchor() {
+        imageListView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(view).multipliedBy(0.3)
+        }
+    }    
 }
 
 extension PhotoPickViewController: ImageViewListViewDelegate {
     func didSeletedPhoto(phImage: PHImage) {
-//        imageProcessingQueue.async {
-//            PHAssetManager.shared.getImage(asset: phImage.asset) { image in
-//                <#code#>
-//            }
-//        }
+        imageProcessingQueue.async {
+            PHAssetManager.shared.getImage(asset: phImage.asset) { image in
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    selectedImageView.setImage(image)
+                }
+            }
+        }
     }
 }
